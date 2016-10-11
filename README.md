cf90aa207191e7b2c0588f00027dc53d27cac8ae
kering <freekering@gmail.com>
2016-10-11 12:30:31 +0900

    initial commit with RidiLog

---
+++ README.md
@@ -0,0 +1,26 @@
+# ridihelper-android
+Convenient helper classes for Android platform
+
+## Getting started
+This library is distributed by [jitpack](https://jitpack.io).
+
+You should add jitpack maven repository to build.gradle file of your root project.
+
+```
+allprojects {
+    repositories {
+        jcenter()
+        maven { url "https://jitpack.io" }
+    }
+}
+```
+
+Then you can include this library by adding dependency script to build.gradle file of your project.
+
+```
+dependencies {
+    ...
+    compile 'com.github.ridibooks:ridihelper-android:1.0' 
+    ...
+}
+```
---
+++ library/src/main/kotlin/com/ridi/viewer/RidiLog.kt
@@ -0,0 +1,78 @@
+package com.ridi.viewer
+
+import android.util.Log
+
+object RidiLog {
+    @JvmStatic
+    fun e(tag: String, e: Throwable?) = e(tag, "", e)
+
+    @JvmOverloads
+    @JvmStatic
+    fun e(tag: String, message: String, e: Throwable? = null) = Log.e(tag, message, e)
+
+    @JvmStatic
+    fun e(clazz: Class<*>, e: Throwable?) = e(clazz, "", e)
+
+    @JvmOverloads
+    @JvmStatic
+    fun e(clazz: Class<*>, message: String, e: Throwable? = null) = e(clazz.simpleName, message, e)
+
+    @JvmStatic
+    fun w(tag: String, e: Throwable?) = w(tag, "", e)
+
+    @JvmOverloads
+    @JvmStatic
+    fun w(tag: String, message: String, e: Throwable? = null) = Log.w(tag, message, e)
+
+    @JvmStatic
+    fun w(clazz: Class<*>, e: Throwable?) = w(clazz, "", e)
+
+    @JvmOverloads
+    @JvmStatic
+    fun w(clazz: Class<*>, message: String, e: Throwable? = null) = w(clazz.simpleName, message, e)
+
+    @JvmStatic
+    fun d(tag: String, e: Throwable?) = d(tag, "", e)
+
+    @JvmOverloads
+    @JvmStatic
+    fun d(tag: String, message: String, e: Throwable? = null) = Log.d(tag, message, e)
+
+    @JvmStatic
+    fun d(clazz: Class<*>, e: Throwable?) = d(clazz, "", e)
+
+    @JvmOverloads
+    @JvmStatic
+    fun d(clazz: Class<*>, message: String, e: Throwable? = null) = d(clazz.simpleName, message, e)
+
+    @JvmStatic
+    fun d(message: String) = d("ridibug", message)
+
+    @JvmStatic
+    fun v(tag: String, e: Throwable?) = v(tag, "", e)
+
+    @JvmOverloads
+    @JvmStatic
+    fun v(tag: String, message: String, e: Throwable? = null) = Log.v(tag, message, e)
+
+    @JvmStatic
+    fun v(clazz: Class<*>, e: Throwable?) = v(clazz, "", e)
+
+    @JvmOverloads
+    @JvmStatic
+    fun v(clazz: Class<*>, message: String, e: Throwable? = null) = v(clazz.simpleName, message, e)
+
+    @JvmStatic
+    fun i(tag: String, e: Throwable?) = i(tag, "", e)
+
+    @JvmOverloads
+    @JvmStatic
+    fun i(tag: String, message: String, e: Throwable? = null) = Log.i(tag, message, e)
+
+    @JvmStatic
+    fun i(clazz: Class<*>, e: Throwable?) = i(clazz, "", e)
+
+    @JvmOverloads
+    @JvmStatic
+    fun i(clazz: Class<*>, message: String, e: Throwable? = null) = i(clazz.simpleName, message, e)
+}
