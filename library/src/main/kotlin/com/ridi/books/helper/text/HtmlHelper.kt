6e737786b810f5af665a49ef27f20040787302df
kgwangrae <k.gwangrae@ridi.com>
2016-10-17 14:40:05 +0900

    Moved HtmlHelper from html to text / renamed fromHtml -> makeSpannedFromHtml

--- library/src/main/kotlin/com/ridi/books/helper/text/HtmlHelper.kt
+++ library/src/main/kotlin/com/ridi/books/helper/text/HtmlHelper.kt
@@ -1,10 +1,10 @@
-package com.ridi.books.helper.html
+package com.ridi.books.helper.text
 
 import android.os.Build
 import android.text.Html
 import android.text.Spanned
 
-fun fromHtmlCompat(htmlText: String): Spanned {
+fun makeSpannedFromHtml(htmlText: String): Spanned {
     return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
         Html.fromHtml(htmlText, Html.FROM_HTML_MODE_LEGACY)
     } else {
