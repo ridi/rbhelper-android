b9bd5022ca4ed45399e317a6a41d47aec299644a
kering <kering@ridi.com>
2016-10-21 15:22:57 +0900

    SimpleAnimationListener만 남기고 AnimationHelper 제거

---
+++ src/main/kotlin/com/ridi/books/helper/view/SimpleAnimationListener.kt
@@ -0,0 +1,14 @@
+package com.ridi.books.helper.view
+
+import android.view.animation.Animation
+
+abstract class SimpleAnimationListener : Animation.AnimationListener {
+    override fun onAnimationStart(animation: Animation) {
+    }
+
+    override fun onAnimationEnd(animation: Animation) {
+    }
+
+    override fun onAnimationRepeat(animation: Animation) {
+    }
+}
