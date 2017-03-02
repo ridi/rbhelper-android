cfba41f4d90ca4b5248dc2d150531780e23db2a1
kering <kering@ridi.com>
2017-03-02 18:50:08 +0900

    Add unit tests for AlphaNumericComparator, HashAlgorithms

---
+++ src/test/kotlin/com/ridi/books/helper/text/AlphaNumericComparatorTest.kt
@@ -0,0 +1,18 @@
+package com.ridi.books.helper.text
+
+import org.junit.Assert
+import org.junit.Test
+
+/**
+ * Created by kering on 2017. 3. 2..
+ */
+class AlphaNumericComparatorTest {
+    @Test
+    fun test() {
+        val sources = arrayOf("2. bar", "a", "b", "a 1", "a 10",
+                "15. baz", "abc", "a 2", "a 3", "a 23", "1. foo")
+        val destination = arrayOf("1. foo", "2. bar", "15. baz",
+                "a", "a 1", "a 2", "a 3", "a 10", "a 23", "abc", "b")
+        Assert.assertArrayEquals(destination, sources.sortedArrayWith(AlphaNumericComparator()))
+    }
+}
---
+++ src/test/kotlin/com/ridi/books/helper/text/HashAlgorithmsTest.kt
@@ -0,0 +1,21 @@
+package com.ridi.books.helper.text
+
+import org.junit.Assert
+import org.junit.Test
+
+/**
+ * Created by kering on 2017. 3. 2..
+ */
+class HashAlgorithmsTest {
+    @Test
+    fun testMd5() {
+        Assert.assertEquals("c63bae4cbedc34653fad00e8d9da4111", "Must Use".md5())
+        Assert.assertEquals("c3c7018f3fdf984d889adeecd93882e9", "Ridibooks".md5())
+    }
+
+    @Test
+    fun testSha1() {
+        Assert.assertEquals("e14c47246f73d243b01f85cf6fcc5bb0c4981c7d", "Must Use".sha1())
+        Assert.assertEquals("6a3d4822768f04b0586d4af4af61ec1542d2320a", "Ridibooks".sha1())
+    }
+}
