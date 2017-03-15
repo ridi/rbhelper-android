1dc8bce8892f6d030349c6f80791dced041ba3b4
kgwangrae <k.gwangrae@ridi.com>
2017-03-15 18:42:34 +0900

    Split LifecycleHelper by class

--- src/main/kotlin/com/ridi/books/helper/activity/VisibleActivityCounter.kt
+++ src/main/kotlin/com/ridi/books/helper/activity/VisibleActivityCounter.kt
@@ -1,27 +1,6 @@
 package com.ridi.books.helper.activity
 
 import android.app.Activity
-import android.app.Application
-import android.os.Bundle
-
-/**
- * Created by kering on 15. 7. 27..
- */
-abstract class SimpleActivityLifecycleCallbacks : Application.ActivityLifecycleCallbacks {
-    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {}
-
-    override fun onActivityStarted(activity: Activity) {}
-
-    override fun onActivityResumed(activity: Activity) {}
-
-    override fun onActivityPaused(activity: Activity) {}
-
-    override fun onActivityStopped(activity: Activity) {}
-
-    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
-
-    override fun onActivityDestroyed(activity: Activity) {}
-}
 
 open class VisibleActivityCounter : SimpleActivityLifecycleCallbacks() {
     /**
