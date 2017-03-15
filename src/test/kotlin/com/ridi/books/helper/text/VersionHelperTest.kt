0a420b98ebea5eda91e18141fd7bad56373a3e5c
kgwangrae <k.gwangrae@ridi.com>
2017-03-15 19:33:42 +0900

    VersionHelperTest : expected <-> actual

--- src/test/kotlin/com/ridi/books/helper/text/VersionHelperTest.kt
+++ src/test/kotlin/com/ridi/books/helper/text/VersionHelperTest.kt
@@ -6,13 +6,13 @@ import org.junit.Test
 class VersionHelperTest {
     @Test
     fun testNormalizeAsSemVer() {
-        Assert.assertEquals("8".normalizeAsSemVer(), "8.0.0")
-        Assert.assertEquals("8.4".normalizeAsSemVer(), "8.4.0")
-        Assert.assertEquals("8.4.12".normalizeAsSemVer(), "8.4.12")
-        Assert.assertEquals("13.72512.12".normalizeAsSemVer(), "13.72512.12")
-        Assert.assertEquals("8.4.12_rc1".normalizeAsSemVer(), "8.4.12")
-        Assert.assertEquals("a".normalizeAsSemVer(), "*")
-        Assert.assertEquals("a.8.1".normalizeAsSemVer(), "*")
-        Assert.assertEquals("*".normalizeAsSemVer(), "*")
+        Assert.assertEquals("8.0.0", "8".normalizeAsSemVer())
+        Assert.assertEquals("8.4.0", "8.4".normalizeAsSemVer())
+        Assert.assertEquals("8.4.12", "8.4.12".normalizeAsSemVer())
+        Assert.assertEquals("13.72512.12", "13.72512.12".normalizeAsSemVer())
+        Assert.assertEquals("8.4.12", "8.4.12_rc1".normalizeAsSemVer())
+        Assert.assertEquals("*", "a".normalizeAsSemVer())
+        Assert.assertEquals("*", "a.8.1".normalizeAsSemVer())
+        Assert.assertEquals("*", "*".normalizeAsSemVer())
     }
 }
