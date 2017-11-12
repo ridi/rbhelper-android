d6bce7b5076ae10a2f0d5eb2c0b0985115bdac0c
kering <kering@ridi.com>
2017-11-12 15:54:14 +0900

    Add `TextDrawable` from https://github.com/devunwired/textdrawable, `ZeroPaddingTextView`.

---
+++ src/main/kotlin/com/ridi/books/helper/view/ZeroPaddingTextView.kt
@@ -0,0 +1,15 @@
+package com.ridi.books.helper.view
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.widget.TextView
+
+class ZeroPaddingTextView
+@JvmOverloads constructor(context: Context, attrs: AttributeSet? = null)
+    : TextView(context, attrs) {
+    override fun onDraw(canvas: Canvas) {
+        canvas.translate(0f, 0f)
+        super.onDraw(canvas)
+    }
+}
