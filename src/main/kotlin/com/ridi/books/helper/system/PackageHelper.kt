db97e15e24bf1d6ef78096538eddf48e257f7a34
Davin Ahn <davin.ahn@ridi.com>
2018-01-16 18:18:13 +0900

    범용성을 헤치지 않는 쪽으로 코드 정리

--- src/main/kotlin/com/ridi/books/helper/system/PackageHelper.kt
+++ src/main/kotlin/com/ridi/books/helper/system/PackageHelper.kt
@@ -10,11 +10,6 @@ fun Context.getPackageVersionCode(`package`: String, enabledOnly: Boolean = true
     try {
         val info = packageManager.getPackageInfo(`package`, 0)
         if (enabledOnly.not() || info.applicationInfo.enabled) {
-            if (`package`.startsWith("com.amazon.webview") && info.versionCode / 100000000 < 1) {
-                return info.versionName.split(".").let { parts ->
-                    if (parts.count() < 5) -1 else parts.drop(2).joinToString("").toInt()
-                }
-            }
             return info.versionCode
         }
     } catch (e: PackageManager.NameNotFoundException) {
@@ -31,10 +26,28 @@ fun Context.getSystemWebViewVersionCode(): Int {
         }
     }
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-        return getPackageVersionCode("com.google.android.webview",
-                Build.VERSION.SDK_INT >= Build.VERSION_CODES.N).let { versionCode ->
-            if (versionCode > 0) versionCode else
-                getPackageVersionCode("com.amazon.webview.chromium")
+        val versionCode = getPackageVersionCode("com.google.android.webview",
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+        if (versionCode > 0) {
+            return versionCode
+        } else {
+            val `package` = "com.amazon.webview.chromium"
+            try {
+                val info = packageManager.getPackageInfo(`package`, 0)
+                return if (info.versionCode / 100000000 > 0) {
+                    info.versionCode
+                } else {
+                    info.versionName.split(".").let { parts ->
+                        if (parts.count() != 5) {
+                            -1
+                        } else {
+                            parts.drop(2).joinToString("").toInt()
+                        }
+                    }
+                }
+            } catch (e: PackageManager.NameNotFoundException) {
+                Log.d("Failed to get $`package` package info", e)
+            }
         }
     }
     return -1
