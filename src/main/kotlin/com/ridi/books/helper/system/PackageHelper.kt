a43bd1d58c8153e5c8398ac44fef7da9daa54aa9
Davin Ahn <davin.ahn@ridi.com>
2018-01-16 17:35:51 +0900

    킨들 기기의 웹뷰 버전 코드를 가져올 수 있도록 기능 확장

--- src/main/kotlin/com/ridi/books/helper/system/PackageHelper.kt
+++ src/main/kotlin/com/ridi/books/helper/system/PackageHelper.kt
@@ -10,6 +10,11 @@ fun Context.getPackageVersionCode(`package`: String, enabledOnly: Boolean = true
     try {
         val info = packageManager.getPackageInfo(`package`, 0)
         if (enabledOnly.not() || info.applicationInfo.enabled) {
+            if (`package`.startsWith("com.amazon.webview") && info.versionCode / 100000000 < 1) {
+                return info.versionName.split(".").let { parts ->
+                    if (parts.count() < 5) -1 else parts.drop(2).joinToString("").toInt()
+                }
+            }
             return info.versionCode
         }
     } catch (e: PackageManager.NameNotFoundException) {
@@ -27,7 +32,10 @@ fun Context.getSystemWebViewVersionCode(): Int {
     }
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
         return getPackageVersionCode("com.google.android.webview",
-                Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.N).let { versionCode ->
+            if (versionCode > 0) versionCode else
+                getPackageVersionCode("com.amazon.webview.chromium")
+        }
     }
     return -1
 }
