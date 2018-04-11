ec2103ed6bd87d780367cf690232cd26da94f943
freekering <freekering@users.noreply.github.com>
2018-04-11 17:07:43 +0900

    Update SoftKeyboardHelper.kt
    - Make `showSoftKeyboard`, `hideSoftKeyboard` forward return value

--- src/main/kotlin/com/ridi/books/helper/view/SoftKeyboardHelper.kt
+++ src/main/kotlin/com/ridi/books/helper/view/SoftKeyboardHelper.kt
@@ -8,14 +8,12 @@ private val Context.inputMethodManager: InputMethodManager
     get() = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
 
 @JvmOverloads
-fun View.showSoftKeyboard(flags: Int = InputMethodManager.SHOW_IMPLICIT) {
-    context.inputMethodManager.showSoftInput(this, flags)
-}
+fun View.showSoftKeyboard(flags: Int = InputMethodManager.SHOW_IMPLICIT) =
+        context.inputMethodManager.showSoftInput(this, flags)
 
 @JvmOverloads
-fun View.hideSoftKeyboard(flags: Int = 0) {
-    context.inputMethodManager.hideSoftInputFromWindow(windowToken, flags)
-}
+fun View.hideSoftKeyboard(flags: Int = 0) =
+        context.inputMethodManager.hideSoftInputFromWindow(windowToken, flags)
 
 @JvmOverloads
 fun Context.toggleSoftKeyboard(showFlags: Int = 0, hideFlags: Int = 0) {
