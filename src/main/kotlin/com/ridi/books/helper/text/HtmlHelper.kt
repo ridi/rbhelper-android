1e7e890b5b025c320aeb061d4c5dfd65d88627cb
YSYoon <ys.yoon@ridi.com>
2019-08-20 15:21:27 +0900

    범용적으로 사용할 수 있도록 TagHandler를 parameter로 받도록 수정

--- src/main/kotlin/com/ridi/books/helper/text/HtmlHelper.kt
+++ src/main/kotlin/com/ridi/books/helper/text/HtmlHelper.kt
@@ -1,33 +1,23 @@
 package com.ridi.books.helper.text
 
 import android.os.Build
+import android.text.Editable
 import android.text.Html
 import android.text.Spanned
+import org.xml.sax.XMLReader
 
 fun makeSpannedFromHtml(htmlText: String): Spanned {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-        Html.fromHtml(htmlText, Html.FROM_HTML_MODE_LEGACY)
-    } else {
-        @Suppress("DEPRECATION")
-        Html.fromHtml(htmlText)
-    }
+    return makeSpannedFromHtml(htmlText, null)
 }
 
-fun makeSpannedFromHtmlWithLtGtPreserved(htmlText: String): Spanned {
-    // EPUB 본문 텍스트에 사용된 &lt; &gt;를 처리하기 위한 목적으로 만들어졌기 때문에 closing인 경우는 고려하지 않음
-    // Close되지 않은 unknown tag에 대해 일괄적으로 closing처리가 되는데 이 경우는 따로 처라히지 않음
+fun makeSpannedFromHtml(
+    htmlText: String,
+    tagHandler: ((opening: Boolean, tag: String, output: Editable, xmlReader: XMLReader) -> Unit)?
+): Spanned {
     return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-        Html.fromHtml(htmlText, Html.FROM_HTML_MODE_LEGACY, null) { opening, tag, output, _ ->
-            if (opening) {
-                output.append("<$tag>")
-            }
-        }
+        Html.fromHtml(htmlText, Html.FROM_HTML_MODE_LEGACY, null, tagHandler)
     } else {
         @Suppress("DEPRECATION")
-        Html.fromHtml(htmlText, null) { opening, tag, output, _ ->
-            if (opening) {
-                output.append("<$tag>")
-            }
-        }
+        Html.fromHtml(htmlText, null, tagHandler)
     }
 }
