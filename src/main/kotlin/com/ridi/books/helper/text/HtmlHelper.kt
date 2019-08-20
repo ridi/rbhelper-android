0a6186ff9e4d9e6f39586feae84ead7117b32ab7
YSYoon <ys.yoon@ridi.com>
2019-08-20 14:28:49 +0900

    `<`, `>`로 감싸져있는 본문 텍스트를 보존할 수 있는 함수 추가

--- src/main/kotlin/com/ridi/books/helper/text/HtmlHelper.kt
+++ src/main/kotlin/com/ridi/books/helper/text/HtmlHelper.kt
@@ -12,3 +12,22 @@ fun makeSpannedFromHtml(htmlText: String): Spanned {
         Html.fromHtml(htmlText)
     }
 }
+
+fun makeSpannedFromHtmlWithLtGtPreserved(htmlText: String): Spanned {
+    // EPUB 본문 텍스트에 사용된 &lt; &gt;를 처리하기 위한 목적으로 만들어졌기 때문에 closing인 경우는 고려하지 않음
+    // Close되지 않은 unknown tag에 대해 일괄적으로 closing처리가 되는데 이 경우는 따로 처라히지 않음
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        Html.fromHtml(htmlText, Html.FROM_HTML_MODE_LEGACY, null) { opening, tag, output, _ ->
+            if (opening) {
+                output.append("<$tag>")
+            }
+        }
+    } else {
+        @Suppress("DEPRECATION")
+        Html.fromHtml(htmlText, null) { opening, tag, output, _ ->
+            if (opening) {
+                output.append("<$tag>")
+            }
+        }
+    }
+}
