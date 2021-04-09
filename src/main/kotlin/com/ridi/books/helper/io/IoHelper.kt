ce2d0cdf78e47132ea4eca1c7824a715bb77433f
YSYoon <ys.yoon@ridi.com>
2021-04-09 15:54:25 +0900

    Use kotlin.io.copyTo to copy File, add File.moveTo(File), use .use {}

--- src/main/kotlin/com/ridi/books/helper/io/IoHelper.kt
+++ src/main/kotlin/com/ridi/books/helper/io/IoHelper.kt
@@ -3,123 +3,105 @@ package com.ridi.books.helper.io
 import com.ridi.books.helper.Log
 import java.io.Closeable
 import java.io.File
-import java.io.FileInputStream
-import java.io.FileOutputStream
 import java.io.IOException
 import java.io.InputStream
 import java.io.ObjectInputStream
 import java.io.ObjectOutputStream
 import java.nio.ByteBuffer
-import kotlin.math.min
+
+private const val IOHELPER_TAG = "IoHelper"
 
 fun File.copyTo(dest: File): Boolean {
-    try {
-        val input = FileInputStream(this)
-        val output = FileOutputStream(dest)
-        val inChannel = input.channel
-        val outChannel = output.channel
+    runCatching {
+        copyTo(dest, overwrite = true)
+    }.let {
+        if (it.isSuccess) return true
 
-        val blockSize = min(512 * 1024 * 1024, inChannel.size())
-        var position = 0L
-        while (outChannel.transferFrom(inChannel, position, blockSize) > 0) {
-            position += blockSize
-        }
+        Log.e(IOHELPER_TAG, "file copy error", it.exceptionOrNull())
+        return false
+    }
+}
 
-        inChannel.close()
-        input.close()
-        outChannel.close()
-        output.close()
-        return true
-    } catch (e: Exception) {
-        Log.e(javaClass, "file copy error", e)
+fun File.moveTo(dest: File): Boolean {
+    runCatching {
+        copyTo(dest, overwrite = true)
+        delete()
+    }.let {
+        if (it.isSuccess) return true
+
+        Log.e(IOHELPER_TAG, "file move error", it.exceptionOrNull())
         return false
     }
 }
 
 @Throws(IOException::class)
 fun File.getBytes(): ByteArray {
-    val input = FileInputStream(this)
-    val channel = input.channel
+    this.inputStream().use { input ->
+        input.channel.use { channel ->
+            val length = length()
+            // Create the byte array to hold the data
+            val bytes = ByteArray(length.toInt())
+            val buffer = ByteBuffer.wrap(bytes)
 
-    val length = length()
-    // Create the byte array to hold the data
-    val bytes = ByteArray(length.toInt())
-    val buffer = ByteBuffer.wrap(bytes)
+            var offset = 0L
+            var numRead: Int
+            do {
+                numRead = channel.read(buffer, offset)
+                offset += numRead
+            } while (offset < length && numRead >= 0)
 
-    var offset = 0L
-    var numRead: Int
-    do {
-        numRead = channel.read(buffer, offset)
-        offset += numRead
-    } while (offset < length && numRead >= 0)
-
-    channel.close()
-    input.close()
-    return bytes
+            return bytes
+        }
+    }
 }
 
 fun File.makeNoMediaFile() {
-    if (isDirectory.not()) {
-        return
-    }
+    if (isDirectory.not()) return
 
     val file = File(this, ".nomedia")
-    if (file.exists()) {
-        return
-    }
+    if (file.exists()) return
 
     try {
         file.createNewFile()
     } catch (e: IOException) {
-        Log.e(javaClass, "file creating error", e)
+        Log.e(IOHELPER_TAG, "file creating error", e)
     }
 }
 
 @Throws(IOException::class)
 fun InputStream.writeStreamToFile(file: File) {
-    var output: FileOutputStream? = null
-    var exception: IOException? = null
-    try {
-        output = FileOutputStream(file)
-        copyTo(output)
-    } catch (e: IOException) {
-        exception = e
-    } finally {
-        closeAndThrowExceptionToReport(output, exception)
-    }
+    runCatching {
+        file.outputStream().use { output ->
+            copyTo(output)
+        }
+    }.onFailure { throw it }
 }
 
 fun Any.saveToFile(file: File) {
-    try {
+    runCatching {
         file.createNewFile()
-        val fileOutput = FileOutputStream(file)
-        val objectOutput = ObjectOutputStream(fileOutput)
-        objectOutput.writeObject(this)
-        objectOutput.flush()
-        objectOutput.close()
-        fileOutput.close()
-    } catch (e: Exception) {
-        Log.e(javaClass, "writing object to file error", e)
-    }
+        file.outputStream().use { fileOutput ->
+            ObjectOutputStream(fileOutput).use { objectOutput ->
+                objectOutput.writeObject(this)
+                objectOutput.flush()
+            }
+        }
+    }.onFailure { Log.e(IOHELPER_TAG, "writing object to file error", it) }
 }
 
 @Suppress("UNCHECKED_CAST")
 fun <T> File.loadObject(): T? {
-    if (exists().not()) {
-        return null
-    }
+    if (exists().not()) return null
 
     var obj: T? = null
 
-    try {
-        val fileInput = FileInputStream(this)
-        val objectInput = ObjectInputStream(fileInput)
-        obj = objectInput.readObject() as T?
-        objectInput.close()
-        fileInput.close()
-    } catch (e: Exception) {
-        Log.e(javaClass, "loading object from file error", e)
-    }
+    runCatching {
+        this.inputStream().use { fileInput ->
+            ObjectInputStream(fileInput).use { objectInput ->
+                obj = objectInput.readObject() as T?
+            }
+        }
+    }.onFailure { Log.e(IOHELPER_TAG, "loading object from file error", it) }
 
     return obj
 }
